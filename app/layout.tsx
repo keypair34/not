0c19485855b286223f541a7259a6b718fe9cb34d
Seto Elkahfi <hej@setoelkahfi.se>
2025-06-05 23:54:18 +0200

    fix wallet layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </head>
       <body>
         <AppRouterCacheProvider>
           <Container>{children}</Container>
