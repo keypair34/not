a7e2642adf5c77b53ffcd66ef0d3f12f2b749ce4
keypair34 <work@setoelkahfi.se>
2025-07-16 14:56:13 +0200

    Enable horizontal scrolling for address text in WearApp

--- wearOSApp/app/src/main/java/xyz/notwallet/NotWallet/presentation/MainActivity.kt
+++ wearOSApp/app/src/main/java/xyz/notwallet/NotWallet/presentation/MainActivity.kt
@@ -1,12 +1,12 @@
 package xyz.notwallet.NotWallet.presentation
 
-
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -105,7 +105,11 @@ fun WearApp() {
                             fontSize = 20.sp,
                             fontFamily = FontFamily.Monospace,
                             color = Color.Black,
-                            modifier = Modifier.padding(8.dp).clip(RectangleShape)
+                            modifier =
+                                    Modifier.padding(8.dp)
+                                            .clip(RectangleShape)
+                                            .horizontalScroll(rememberScrollState()),
+                            maxLines = 1
                     )
                 }
             }
